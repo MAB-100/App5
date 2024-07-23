d07e8c5e72453c5ef20a5888e860066e0f1675d5
MAB-100 <shahabbas661@gmail.com>
2024-07-23 20:26:42 +0500

    added link and other quality of life changes

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
 import send_email
 
+topic = "tesla"
 api_key = "39b707878e344e8c9e77b5e33cfa2dff"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-06-23&" \
-    "sortBy=publishedAt&apiKey=39b707878e344e8c9e77b5e33cfa2dff"
+url = f"https://newsapi.org/v2/everything?q={topic}&from=2024-06-23&sortBy=publishedAt&apiKey=39b707878e344e8c9e77b5e33cfa2dff&language=en"
 
 
 request = requests.get(url)
@@ -13,9 +13,10 @@ content = request.json()
 
 body = ""
 
-for articles in content['articles']:
+for articles in content['articles'][:20]:
     if articles['title'] != None:
-        body = body + articles['title'] + "\n" + articles['description'] + "\n\n"
+        body = "Subject: Today's news" + "\n" + body + articles['title'] + "\n" + articles['description'] +"\n" + articles["url"] + "\n\n"
+
 
 
 body = body.encode("utf-8")
